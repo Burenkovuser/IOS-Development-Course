dac35cfb5c9db99e217c2e5df22225729bfe3562
Burenkovuser <nb2207@yandex.ru>
2017-01-15 19:21:32 +0600

    Initial Commit

---
+++ DZ19 UIView/ViewController.m
@@ -0,0 +1,124 @@
+//
+//  ViewController.m
+//  DZ19 UIView
+//
+//  Created by Vasilii on 14.01.17.
+//  Copyright © 2017 Vasilii Burenkov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+//энум, перечисление для клеток и фигур
+typedef enum {
+    TagBlackCell,
+    TagWhiteCell,
+    TagWhiteChecker,
+    TagRedChecker,
+    
+}TagCell;
+
+@interface ViewController ()
+
+@property (strong, nonatomic) UIView *chessBoard;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //устанавливается цвет фон нашего вью
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    // инициализируем шахматную доску
+    self.chessBoard = [[UIView alloc] initWithFrame:CGRectInset(self.view.bounds, 0, 0)];
+    
+    //минимальная сторона шахматной доски равна самой минимальной доске во внутренних границах по ширине и высоте
+    CGFloat minSideChessBoard = MIN(CGRectGetWidth(self.chessBoard.bounds), CGRectGetHeight(self.chessBoard.bounds));
+    
+    //шахматная доска в форме квадрата с минимальной стороной
+    self.chessBoard.bounds = CGRectMake(self.chessBoard.bounds.origin.x, self.chessBoard.bounds.origin.y, minSideChessBoard, minSideChessBoard);
+    
+    self.chessBoard.backgroundColor = [UIColor whiteColor];
+    
+    //создаются клетки и фигуры
+    [self createCellsAndCheckers: self.chessBoard];
+    
+    [self.view addSubview:self.chessBoard];
+}
+
+-(void) createCellsAndCheckers: (UIView*) chessBoard {
+    for (int y = 0; y < 8; y += 1) {
+        for (int x = 0; x < 8 ; x += 1) {
+            CGRect cellFrame;
+            //размер 1/8
+            cellFrame.size = CGSizeMake(CGRectGetWidth(chessBoard.bounds) / 8, CGRectGetMaxX(chessBoard.bounds) / 8);
+            //начало координат
+            cellFrame.origin = CGPointMake(x * CGRectGetWidth(cellFrame), y * CGRectGetHeight(cellFrame));
+            //инициализируем ячейку
+            UIView * cell = [[UIView alloc] initWithFrame:cellFrame];
+            
+            //определяем цвет ячейки
+            cell.backgroundColor = (x + y) % 2 == 0 ? [UIColor whiteColor] : [UIColor blackColor];
+            cell.tag = (x + y) % 2 == 0 ? TagWhiteCell : TagBlackCell;
+            
+            //создаем ячейки на шахматной доске
+            [self.chessBoard addSubview:cell];
+            
+            //расставляем шашки по клеткам доски
+            if ((y <= 2 || y >= 5) && (x+y) % 2 != 0) {
+                //создаем фигуру шашки
+                CGRect checkFrame = CGRectInset(cellFrame, 10, 10);
+                //инициализируем, задаем цевет
+                UIView *checker = [[UIView alloc] initWithFrame:checkFrame];
+                checker.backgroundColor = y <= 3 ? [UIColor whiteColor] : [UIColor redColor];
+                
+                // скругляем вью шашки
+                checker.layer.cornerRadius = checker.frame.size.width /2;
+                [chessBoard addSubview:checker];
+            }
+        }
+    }
+}
+// метод который поддерживает маски всех ориентациий
+-(UIInterfaceOrientationMask) supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskAll;
+}
+
+-(UIColor*) randomColor {
+    return [UIColor colorWithRed:0.1 + (arc4random_uniform(100) * 0.01) green:0.1 + (arc4random_uniform(100) * 0.01) blue: 0.1 + (arc4random_uniform(100) * 0.01) alpha:0.4];
+}
+//метод перетасовыающий шашки и меняющий цвет черных клеток
+-(void) shuffleCheckersAndColorBlackCells: (UIView*) chessBoard {
+   
+    UIColor *color = [self randomColor];
+    //анимаиция с задержекой 3 секунды
+    [UIView animateWithDuration:3 animations:^{
+        for (UIView * cell in self.chessBoard.subviews) {
+            if (cell.tag == TagBlackCell) { // если имя (метка) ячейки
+                cell.backgroundColor = color;
+            }
+            
+            CGPoint whiteChecherOrigin;
+            CGPoint redCheckerOrigin;
+            
+            // рендомный индекс для шашек
+            NSInteger indexWhiteChecker = arc4random_uniform((UInt32)chessBoard.subviews.count - 1);
+            NSInteger indexRedChecker = arc4random_uniform((UInt32)chessBoard.subviews.count - 1);
+            
+            //через цикл while ищем в self.chessBoard.subviews красную шашку
+            while (![[chessBoard.subviews objectAtIndex: indexWhiteChecker] viewWithTag:TagWhiteChecker]) {
+                indexWhiteChecker = arc4random_uniform((UInt32)self.chessBoard.subviews.count - 1 );
+            }
+        }
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
